f6a2c25b42504a60744c2dbabd90a83f82c5684c
Austin <copeman.austin@gmail.com>
2015-07-29 10:49:53 -0400

    Update
    Added new Cauresel

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -460,6 +460,27 @@ if (typeof jQuery === 'undefined') {
   }
 
 
+    setCarouselHeight('#carousel-resume');
+
+    function setCarouselHeight(id)
+    {
+        var slideHeight = [];
+        $(id+' .item').each(function()
+        {
+            // add all slide heights to an array
+            slideHeight.push($(this).height());
+        });
+
+        // find the tallest item
+        max = Math.max.apply(null, slideHeight);
+
+        // set the slide's height
+        $(id+' .carousel-content').each(function()
+        {
+            $(this).css('height',max+'px');
+        });
+    }
+
   // CAROUSEL PLUGIN DEFINITION
   // ==========================
 
